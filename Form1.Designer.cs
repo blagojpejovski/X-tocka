370d4f032d0494dccf75199a9939c411a807bf7d
blagojpejovski <67788160+blagojpejovski@users.noreply.github.com>
2020-07-03 14:36:15 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace X_tocka_proekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.Xwin = new System.Windows.Forms.Label();
+            this.Draw = new System.Windows.Forms.Label();
+            this.Owin = new System.Windows.Forms.Label();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(261, 75);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 64);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(261, 145);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 64);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(261, 215);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 64);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(358, 215);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 64);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(358, 145);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 64);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(358, 75);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 64);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(459, 215);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 64);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(459, 145);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 64);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(459, 75);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 64);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.allButtonClicks);
+            // 
+            // Xwin
+            // 
+            this.Xwin.Location = new System.Drawing.Point(258, 49);
+            this.Xwin.Name = "Xwin";
+            this.Xwin.Size = new System.Drawing.Size(100, 23);
+            this.Xwin.TabIndex = 9;
+            this.Xwin.Text = "label1";
+            // 
+            // Draw
+            // 
+            this.Draw.Location = new System.Drawing.Point(364, 49);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(78, 17);
+            this.Draw.TabIndex = 10;
+            this.Draw.Text = "label2";
+            this.Draw.Click += new System.EventHandler(this.Draw_Click);
+            // 
+            // Owin
+            // 
+            this.Owin.Location = new System.Drawing.Point(459, 49);
+            this.Owin.Name = "Owin";
+            this.Owin.Size = new System.Drawing.Size(91, 17);
+            this.Owin.TabIndex = 11;
+            this.Owin.Text = "label3";
+            this.Owin.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Location = new System.Drawing.Point(261, 303);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(97, 44);
+            this.NewGameButton.TabIndex = 12;
+            this.NewGameButton.Text = "New Game";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(437, 303);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(97, 44);
+            this.ResetButton.TabIndex = 13;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(351, 353);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(91, 44);
+            this.ExitButton.TabIndex = 14;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 477);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.NewGameButton);
+            this.Controls.Add(this.Owin);
+            this.Controls.Add(this.Draw);
+            this.Controls.Add(this.Xwin);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Reset";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Label Xwin;
+        private System.Windows.Forms.Label Draw;
+        private System.Windows.Forms.Label Owin;
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_tocka_proekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int player = 2;//broj na igrachi
+        public int turn = 0; //broj odigrani potezi
+        public int neresheno = 0;
+        public int pobediX = 0;
+        public int pobediO = 0;//cuvanje br na pobedi na X, O i kolku neresheni rezulati
+        private void allButtonClicks(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text == "")//ako e nema ovaa proverka, so povtoren klik na kvadratceto ke se smeni X vo O i obratno
+            {
+                if (player % 2 == 0)
+                {
+                    button.Text = "X";//player 1 pocnuva so X
+                    player++;
+                    turn++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    player++;
+                    turn++;
+                }
+                if(isDraw()==true)
+                {
+                    neresheno++;
+                    MessageBox.Show("DRAW !");
+                    newGame();
+
+                }
+                if(Winner()==true)
+                {
+                    if(button.Text=="X")
+                    {
+                        pobediX++;
+                        MessageBox.Show("PLAYER 1 WINS !");
+                        newGame();
+                    }
+                    else
+                    {
+                        pobediO++;
+                        MessageBox.Show("PLAYER 2 WINS !");
+                        newGame();
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Xwin.Text = "Player 1: " + pobediX;
+            Draw.Text = "Draw: " + neresheno;
+            Owin.Text = "Player 2: " + pobediO;
+        }
+        bool Winner()
+        {
+            //vertikalno
+            if (btn1.Text != "" && (btn1.Text == btn2.Text) && (btn2.Text == btn3.Text))
+                return true;
+            else if (btn4.Text != "" && (btn4.Text == btn5.Text) && (btn5.Text == btn6.Text))
+                return true;
+            if (btn7.Text != "" && (btn7.Text == btn8.Text) && (btn8.Text == btn9.Text))
+                return true;
+            //horizontalno
+            if (btn1.Text != "" && (btn1.Text == btn4.Text) && (btn4.Text == btn7.Text))
+                return true;
+            else if (btn2.Text != "" && (btn2.Text == btn5.Text) && (btn5.Text == btn8.Text))
+                return true;
+            else if (btn3.Text != "" && (btn3.Text == btn6.Text) && (btn6.Text == btn9.Text))
+                return true;
+            //dijagonalno
+            if (btn1.Text != "" && (btn1.Text == btn5.Text) && (btn5.Text == btn9.Text))
+                return true;
+            else if (btn7.Text != "" && (btn7.Text == btn5.Text) && (btn5.Text == btn3.Text))
+                return true;
+            else
+                return false;
+        }
+        bool isDraw()
+        {
+            if ((turn == 9) && Winner() == false)//klucna proverka, bidejki ako stasa do 9 potezi, i da ima pobednik ke vadi neresheno
+                return true;
+            else return false;
+        }
+        void newGame()
+        {
+            //cuvaj gi prethodnite rezultati
+            Xwin.Text = "Player 1: " + pobediX;
+            Draw.Text = "Draw: " + neresheno;
+            Owin.Text = "Player 2: " + pobediO;
+            btn1.Text = btn2.Text = btn3.Text = btn4.Text = btn5.Text = btn6.Text = btn7.Text = btn8.Text = btn9.Text = "";//gi resetirame site polinja(prazni)
+            player = 2;
+            turn = 0;//resetirame potezi(bitno za broenje na neresheno)
+
+        }
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            newGame();
+        }
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            neresheno = 0;
+            pobediO = 0;
+            pobediX = 0;
+            newGame();
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Draw_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
